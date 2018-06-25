74345605e444c08ad798595956bb19e9ec2ca5de
GeeMajor <40342776+GeeMajor@users.noreply.github.com>
2018-06-24 21:52:34 -0500

    Login egg-shell

---
+++ main/java/com/ezlife/bebetter/Login.java
@@ -0,0 +1,28 @@
+package com.ezlife.bebetter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    private Button btRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btRegister = (Button) findViewById(R.id.Register);
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                CallServices()
+            }
+        });
+    }
+
+    protected void CallServices(){
+
+    }
+}
